1448df905736113e18f42f6016a1d03a2ee3a246
joanracenet@gmail.com <joanracenet@gmail.com@bfb192df-59d5-e310-3f69-618d71160846>
2012-11-22 16:23:07 +0000

    AgentStreet : - Ajout des vars - Ajout des getters & setters

--- s9/projet-tutore/ufrst.gaml.extensions.citysimulation/src/ufrst/gaml/skills/CityMovingSkill.java
+++ s9/projet-tutore/ufrst.gaml.extensions.citysimulation/src/ufrst/gaml/skills/CityMovingSkill.java
@@ -12,13 +12,12 @@ import msi.gaml.types.IType;
 @skill(name = "citymoving")
 public class CityMovingSkill extends MovingSkill 
 {
-	@action(name="driveto", args={
-			@arg(name="target", type="service", optional=false),
-			@arg(name="on", type=IType.GRAPH_STR)
-	})
-	public boolean driveTo(final IScope scope) throws GamaRuntimeException
-	{
-		return true;
-	}
+        @action(name="driveto", args={
+                        @arg(name="target", type="service", optional=false),
+                        @arg(name="on", type=IType.GRAPH_STR)
+        })
+        public boolean driveTo(final IScope scope) throws GamaRuntimeException
+        {
+                return true;
+        }
 }
-
--- s9/projet-tutore/ufrst.gaml.extensions.citysimulation/src/ufrst/gaml/species/AgentStreet.java
+++ s9/projet-tutore/ufrst.gaml.extensions.citysimulation/src/ufrst/gaml/species/AgentStreet.java
@@ -17,7 +17,11 @@ import msi.gama.metamodel.population.IPopulation;
 import msi.gama.metamodel.shape.GamaShape;
 import msi.gama.metamodel.shape.IShape;
 import msi.gama.precompiler.GamlAnnotations.action;
+import msi.gama.precompiler.GamlAnnotations.getter;
+import msi.gama.precompiler.GamlAnnotations.setter;
 import msi.gama.precompiler.GamlAnnotations.species;
+import msi.gama.precompiler.GamlAnnotations.var;
+import msi.gama.precompiler.GamlAnnotations.vars;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
 import msi.gama.util.GamaPath;
@@ -26,6 +30,7 @@ import msi.gaml.factories.DescriptionFactory;
 import msi.gaml.species.GamlSpecies;
 import msi.gaml.statements.AspectStatement;
 import msi.gaml.statements.IStatement;
+import msi.gaml.types.IType;
 
 /**
  * Agent for streets. <br/> 
@@ -36,38 +41,79 @@ import msi.gaml.statements.IStatement;
  *
  */
 @species(name="Street")
+@vars({@var(name = "waytype", type = IType.STRING_STR, init = "DOUBLE"), 
+	@var(name = "idstart", type = IType.INT_STR, init = "-1"),
+	@var(name = "idend", type = IType.INT_STR, init = "-1")})
 public class AgentStreet extends GamlAgent
 {
-	
-	/**
-	 * Flag for one-way streets
-	 * True if this is a one-way streets, false otherwise
-	 */
-	private boolean oneWay;
+        
+        /**
+         * Flag for one-way streets
+         * True if this is a one-way streets, false otherwise
+         */
+        private boolean oneWay = false;
 
-	
-	public AgentStreet(ISimulation sim, IPopulation s) throws GamaRuntimeException 
-	{
-		super(sim, s);
-	}
+        /** Identifier of the node of the beginning of the street (used if the street is a one-way)*/
+        private int idStart;
+        
+        /** Identifier of the node of the end of the street (used if the street is a one-way)*/
+        private int idEnd;
+        
+        public AgentStreet(ISimulation sim, IPopulation s) throws GamaRuntimeException 
+        {
+                super(sim, s);
+        }
 
-
-
-
-
-
-
-	/**
-	 * Is it a one-way street ?
-	 * 
-	 * @param scope
-	 * @return True is this is a one-way streets, false otherwise
-	 * @throws GamaRuntimeException
-	 */
-	@action(name="isOneWay")
-	public boolean isOneWay(final IScope scope) throws GamaRuntimeException {
-		return oneWay;
-	}
+        /**
+         * GETTERS & SETTERS used in GAML
+         */
+        @setter("waytype")
+        public void setWayType(String isOneWay)
+        {
+        	//TODO [DEBUG]
+        	System.out.println("Set my way type ");
+        	oneWay = (isOneWay.equalsIgnoreCase("UNIQUE"));        		
+        }
+                
+        @getter("waytype")
+        public String getWayType()
+        {
+        	//TODO [DEBUG]
+        	System.out.println("Get my way type ");
+        	return (oneWay)? "UNIQUE" : "DOUBLE";
+        }
+        
+        @setter("idstart")
+        public void setIdStart(int start)
+        {
+        	//TODO [DEBUG]
+        	System.out.println("Set my idStart with : " + start);
+        	idStart = start;        	
+        }
+        
+        @getter("idstart")
+        public int getIdStart()
+        {
+        	//TODO [DEBUG]
+        	System.out.println("Get my idStart");
+        	return idStart;
+        }
+        
+        @setter("idend")
+        public void setIdEnd(int end)
+        {
+        	//TODO [DEBUG]
+        	System.out.println("Set my idEnd with : " + end);
+        	idEnd = end;
+        }
+        
+        @getter("idend")
+        public int getIdEnd()
+        {
+        	//TODO [DEBUG]
+        	System.out.println("Get my idEnd");
+        	return idEnd;
+        }
 
 
 }
