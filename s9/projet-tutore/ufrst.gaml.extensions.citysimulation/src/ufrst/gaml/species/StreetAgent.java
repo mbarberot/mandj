a3cd06feea757825ea6332bceb549b0ff350ddca
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com@bfb192df-59d5-e310-3f69-618d71160846>
2012-12-07 08:13:58 +0000

    Début du StreetStatement

--- s9/projet-tutore/ufrst.gaml.extensions.citysimulation/src/ufrst/gaml/species/StreetAgent.java
+++ s9/projet-tutore/ufrst.gaml.extensions.citysimulation/src/ufrst/gaml/species/StreetAgent.java
@@ -3,12 +3,17 @@ package ufrst.gaml.species;
 import msi.gama.kernel.simulation.ISimulation;
 import msi.gama.metamodel.agent.GamlAgent;
 import msi.gama.metamodel.population.IPopulation;
+import msi.gama.metamodel.shape.GamaPoint;
+import msi.gama.metamodel.shape.GamaShape;
+import msi.gama.metamodel.shape.IShape;
 import msi.gama.precompiler.GamlAnnotations.getter;
 import msi.gama.precompiler.GamlAnnotations.setter;
 import msi.gama.precompiler.GamlAnnotations.species;
 import msi.gama.precompiler.GamlAnnotations.var;
 import msi.gama.precompiler.GamlAnnotations.vars;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
+import msi.gama.util.GamaList;
+import msi.gama.util.IList;
 import msi.gaml.types.IType;
 
 /**
@@ -51,35 +56,18 @@ public class StreetAgent extends GamlAgent
         @setter("waytype")
         public void setWayType(String isOneWay)
         {
-        	//TODO [DEBUG]
-        	System.out.println("Set my way type ");
-        	System.out.println("----------\n"+ isOneWay);
         	oneWay = (isOneWay.equalsIgnoreCase("UNIQUE"));
-        	System.out.println("waytype = " + oneWay + "\n---------------");
         }
                 
         @getter("waytype")
         public String getWayType()
         {
-        	//TODO [DEBUG]
-        	System.out.println("Get my way type ");
-        	
-        	
-        	/*if(this.getAttribute("waytype") == null)
-        	{
-        		System.out.println("GetWayType Attribute == null");
-        		return "DOUBLE";
-        	}*/
-        		
-        	System.out.println("waytype = " + oneWay );
         	return (oneWay)? "UNIQUE" : "DOUBLE";
         }
         
         @setter("idstart")
         public void setIdStart(int start)
         {
-        	//TODO [DEBUG]
-        	System.out.println("Set my idStart with : " + start);
         	idStart = start;   
         	this.setAttribute("idstart", start);
         }
@@ -87,16 +75,12 @@ public class StreetAgent extends GamlAgent
         @getter("idstart")
         public int getIdStart()
         {
-        	//TODO [DEBUG]
-        	System.out.println("Get my idStart");
         	return idStart;
         }
         
         @setter("idend")
         public void setIdEnd(int end)
         {
-        	//TODO [DEBUG]
-        	System.out.println("Set my idEnd with : " + end);
         	idEnd = end;
         	this.setAttribute("idend", end);
         }
@@ -104,8 +88,6 @@ public class StreetAgent extends GamlAgent
         @getter("idend")
         public int getIdEnd()
         {
-        	//TODO [DEBUG]
-        	System.out.println("Get my idEnd");
         	return idEnd;
         }
 
